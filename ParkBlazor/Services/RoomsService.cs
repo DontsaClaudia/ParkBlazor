875f30aec445820ec5350a9eacf706304c28c4ab
DontsaClaudia <donstsaclaudia@gmail.com>
2024-06-01 03:56:28 +0200

    cruds opérations rooms modèle

--- ParkBlazor/Services/RoomsService.cs
+++ ParkBlazor/Services/RoomsService.cs
@@ -14,31 +14,31 @@ namespace ParkBlazor.Services
 
         public async Task<List<Rooms>> GetAllRoomsAsync()
         {
-            return await _httpClient.GetFromJsonAsync<List<Rooms>>("api/rooms");
+            return await _httpClient.GetFromJsonAsync<List<Rooms>>("https://localhost:7227/api/Rooms");
         }
 
         public async Task<Rooms> GetRoomByIdAsync(int id)
         {
-            return await _httpClient.GetFromJsonAsync<Rooms>($"api/rooms/{id}");
+            return await _httpClient.GetFromJsonAsync<Rooms>($"https://localhost:7227/api/Rooms/{id}");
         }
 
         public async Task<Rooms> AddRoomAsync(Rooms room)
         {
-            var response = await _httpClient.PostAsJsonAsync("api/rooms", room);
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:7227/api/Rooms", room);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<Rooms>();
         }
 
         public async Task<Rooms> UpdateRoomAsync(Rooms room)
         {
-            var response = await _httpClient.PutAsJsonAsync($"api/rooms/{room.Id}", room);
+            var response = await _httpClient.PutAsJsonAsync($"https://localhost:7227/api/Rooms/{room.Id}", room);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<Rooms>();
         }
 
         public async Task<HttpStatusCode> DeleteRoomAsync(int id)
         {
-            var response = await _httpClient.DeleteAsync($"api/rooms/{id}");
+            var response = await _httpClient.DeleteAsync($"https://localhost:7227/api/Rooms/{id}");
             return response.StatusCode;
         }
 
@@ -52,7 +52,7 @@ namespace ParkBlazor.Services
                 queryParams.Add($"parksId={parksId.Value}");
 
             var queryString = string.Join("&", queryParams);
-            return await _httpClient.GetFromJsonAsync<List<Rooms>>($"api/rooms/search?{queryString}");
+            return await _httpClient.GetFromJsonAsync<List<Rooms>>($"https://localhost:7227/api/Rooms/search?{queryString}");
         }
     }
 }
